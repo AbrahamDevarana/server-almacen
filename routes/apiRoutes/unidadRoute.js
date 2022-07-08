68efcc56d17e9b8a27d214d85d6ea10ad4aa075e
Abraham <abrahamag93@gmail.com>
2022-07-08 12:53:26 -0500

    Unidades de medida all

--- routes/apiRoutes/unidadRoute.js
+++ routes/apiRoutes/unidadRoute.js
@@ -4,8 +4,19 @@ const unidadController = require('../../controllers/unidadController')
 
 router.get('/', unidadController.getUnidades)
 router.get('/:id', unidadController.getUnidad)
-router.post('/', [check('nombre').not().isEmpty()], unidadController.createUnidad)
-router.put('/:id', [check('nombre').not().isEmpty()], unidadController.updateUnidad)
+router.post('/', 
+    [
+        check('nombre').not().isEmpty(),
+        check('nombreCorto').not().isEmpty(),
+        check('status').not().isEmpty()
+    ], 
+unidadController.createUnidad)
+router.put('/:id', 
+    [
+        check('nombre').not().isEmpty(),
+        check('nombreCorto').not().isEmpty(),
+        check('status').not().isEmpty()
+    ], unidadController.updateUnidad)
 router.delete('/:id', unidadController.deleteUnidad)
 
 
