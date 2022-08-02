9c193a956d99c21acb4802f6188b380444b78c57
Abraham <abrahamag93@gmail.com>
2022-08-02 18:21:58 -0500

    Permisos

--- controllers/permisoController.js
+++ controllers/permisoController.js
@@ -1,5 +1,6 @@
 const { Role } = require('../models')
 const Permiso = require('../models/Permisos')
+const Users = require('../models/Users')
 
 exports.getPermisos = async (req, res) => {
     try {
@@ -19,21 +20,24 @@ exports.getPermisos = async (req, res) => {
 exports.getPermiso = async ( req, res ) => {
     
     const {id} = req.params
-    const {tipoUsuario_id} = req.user
     
     try{
         
-        // obtener permisos por roles de la tabla pivote
-        await Permiso.findAll({ include: { model: Role, where: { id:tipoUsuario_id } } })
-        .then(permisos => {
-            if(permisos){
-                res.status(200).json({ permisos })
-            }
-        })
-        .catch(error => {
-            res.status(500).json({ message: 'Error al obtener los permisos', error: error.message })
-        })
+        await Users.findOne({ where: { id }}).then( async user => {
+            // obtener permisos por roles de la tabla pivote
+            await Permiso.findAll({ include: { model: Role, where: { id:user.tipoUsuario_id } } })
+            .then(permisos => {
+                if(permisos){
+                    res.status(200).json({ permisos })
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'Error al obtener los permisos', error: error.message })
+            })
 
+        }).catch( (error) => {
+            res.status(500).json({ message: 'Error al obtener los permisos del usuario', error: error.message })
+        })
 
 
 
